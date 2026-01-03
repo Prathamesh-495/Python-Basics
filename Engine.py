9efdb24b893aaa02c3952c7823801fdfa507f983
Prathamesh <prathameshpande632@gmail.com>
2026-01-03 12:28:03 +0530

    sixth commit

--- Engine.py
+++ Engine.py
@@ -1,19 +1,38 @@
-dummy = "hello world"
-print(dummy)
-print(type(dummy))
+# dummy = "hello world"
+# print(dummy)
+# print(type(dummy))
 
-name = input("enter your name :")
-print(name)
-print(type(name))
+# name = input("enter your name :")
+# print(name)
+# print(type(name))
 
-text = input("Enter some text : ")
-print(text.upper())
-print(text.lower())
-print(text.title())
-print(text.find('e'))
-print(text.count('a'))
-print(text.replace('a','@'))
-print(len(text))
+# text = input("Enter some text : ")
+# print(text.upper())
+# print(text.lower())
+# print(text.title())
+# print(text.find('e'))
+# print(text.count('a'))
+# print(text.replace('a','@'))
+# print(len(text))
 
-x = int(input("Enter number within range 10 : "))
-print(x>=3 and x<=10)
+# x = int(input("Enter number within range 10 : "))
+# print(x>=3 and x<=10)
+
+courses = ['ba','bcom','bsc','ma','mcom','msc']
+print(courses)
+print(courses[0])
+print(courses[1:3])
+print(courses[-1])
+
+courses.append('BE')
+print(courses)
+courses.insert(3,'ME')
+print(courses)
+
+new = ['11th','12th']
+courses.append(new)
+print(courses)
+courses.extend(new)
+print(courses)
+for items in courses:
+    print(items)
