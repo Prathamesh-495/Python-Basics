ff6db20412bcf786294719ee20b9ed4e72f67d4d
Prathamesh <prathameshpande632@gmail.com>
2026-01-03 12:41:02 +0530

    4th colab

--- Engine.py
+++ Engine.py
@@ -18,21 +18,79 @@
 # x = int(input("Enter number within range 10 : "))
 # print(x>=3 and x<=10)
 
-courses = ['ba','bcom','bsc','ma','mcom','msc']
-print(courses)
-print(courses[0])
-print(courses[1:3])
-print(courses[-1])
+# courses = ['ba','bcom','bsc','ma','mcom','msc']
+# print(courses)
+# print(courses[0])
+# print(courses[1:3])
+# print(courses[-1])
 
-courses.append('BE')
-print(courses)
-courses.insert(3,'ME')
-print(courses)
+# courses.append('BE')
+# print(courses)
+# courses.insert(3,'ME')
+# print(courses)
 
-new = ['11th','12th']
-courses.append(new)
-print(courses)
-courses.extend(new)
-print(courses)
-for items in courses:
-    print(items)
+# new = ['11th','12th']
+# courses.append(new)
+# print(courses)
+# courses.extend(new)
+# print(courses)
+
+# for items in courses:
+#     print(items)
+
+
+# Tuple definition
+courses = ('ba', 'bcom', 'bsc')
+print(courses, type(courses))
+
+# Access elements
+print(courses[1], courses[3-1])
+print(courses[3:5], courses[-3:])
+
+# Functions: count() and index()
+courses = ('ba','bcom','bsc','bsc','ba')
+print(courses.count('bsc'), courses.index('bsc'))
+
+# Iterating tuples
+for item in courses:
+    print(item)
+for idx, item in enumerate(courses):
+    print(idx, item)
+
+# Length
+print(len(courses))
+
+# Mixed data types
+data = ('ba', 2, True, 'bsc')
+for item in data:
+    print(item, type(item))
+
+# Single-element tuple
+single1 = ('50')   # string
+single2 = ('50',)  # tuple
+print(type(single1), type(single2))
+
+# Combining tuples
+print(('a','b') + (1,2))
+print(('50',) + ('apple',) + ('mango','orange'))
+
+# Membership check
+fruits = ('apple','mango')
+print('mango' in fruits, 'mangoes' in fruits)
+
+# Max, Min
+print(max(2,1,3,7), min(2,1,3,7))
+
+# List → Tuple
+num_list = [1,2,3,4]
+num_tuple = tuple(num_list)
+print(num_list, num_tuple)
+
+# Negative indexing
+num = (1,2,3,4,5,6,7)
+print(num[-1], num[-2])
+print(num[6])
+
+# Tuple slicing
+courses = ('ba','bcom','bsc','be','ma','mcom','msc','me')
+print(courses[3:5], courses[3:], courses[:3], courses[-3:])
